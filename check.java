682614775b120326336a1a2dbbc272ea203a09b0
Ciresh-creates <rc90038@gmail.com>
2025-07-07 19:16:28 +0545

    Update check.java

--- check.java
+++ check.java
@@ -28,7 +28,7 @@ public class check extends JFrame{
 
     public void checkId(){
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/coorporative","root","Appletomypie99");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/coorporative","root","Admin");
             Statement stn = conn.createStatement();
             String qry = "SELECT * FROM customer_data";
             ResultSet rs = stn.executeQuery(qry);
